5f1b41055db2eb2582bad99167d3045f0be3bd43
moisesuailab <157806024+moisesuailab@users.noreply.github.com>
2024-05-23 03:02:32 +0000

    Pending changes exported from your codespace

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import "./shared/utils/TraducoesYup";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { GlobalStyles } from "./shared/styles/global.styles";
 import { AppThemeProvider } from "./shared/contexts";
 import { AuthProvider } from "./shared/contexts/AuthContext";
 
-import "./shared/utils/TraducoesYup";
 
 export const App = () => {
   return (
